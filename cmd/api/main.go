062c3c811381ccec860e088296c1577c70ab9123
JayTailor45 <tailorj64@gmail.com>
2026-04-25 17:01:15 +0530

    chore(db): add db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/JayTailor45/go-social/internal/db"
 	"github.com/JayTailor45/go-social/internal/env"
 	"github.com/JayTailor45/go-social/internal/store"
 	"github.com/joho/godotenv"
@@ -16,9 +17,29 @@ func main() {
 
 	conf := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
-	store := store.NewPostgresStorage(nil)
+	db, err := db.New(
+		conf.db.addr,
+		conf.db.maxOpenConns,
+		conf.db.maxIdleConns,
+		conf.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewPostgresStorage(db)
 
 	app := &application{
 		config: conf,
