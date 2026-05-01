8d3d10ac6d350d06128638e95ecaba3bc88c51dc
JayTailor45 <tailorj64@gmail.com>
2026-05-01 19:50:13 +0530

    feat(post): get post by id support

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/JayTailor45/go-social/internal/store"
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
@@ -40,4 +43,33 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 		WriteJsonError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		WriteJsonError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx := r.Context()
+
+	post, err := app.store.Posts.GetById(ctx, int64(id))
+
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			WriteJsonError(w, http.StatusNotFound, err.Error())
+		default:
+			WriteJsonError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := WriteJSON(w, http.StatusOK, post); err != nil {
+		WriteJsonError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 }
