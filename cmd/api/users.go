be9c09010ed7c3b8f2c24397250531afd2474f39
JayTailor45 <tailorj64@gmail.com>
2026-05-18 23:06:27 +0530

    feat(users): add ability to follow and unfollow users

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"strconv"
 
@@ -8,27 +9,93 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
-	if err != nil {
-		app.badRequestError(w, r, err)
-		return
-	}
+type userCtxKey string
 
-	user, err := app.store.Users.GetById(r.Context(), userID)
-	if err != nil {
-		switch err {
-		case store.ErrNotFound:
-			app.badRequestError(w, r, err)
-			return
-		default:
-			app.internalServerError(w, r, err)
-			return
-		}
-	}
+const userCtx userCtxKey = "user"
+
+func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromContext(r)
 
 	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
+}
+
+type FollowUser struct {
+	UserID int64 `json:"user_id"`
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	followerUser := getUserFromContext(r)
+
+	// TODO: revert with user id from auth
+	var payload FollowUser
+	if err := ReadJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := app.store.Followers.Follow(r.Context(), followerUser.ID, payload.UserID); err != nil {
+		switch err {
+		case store.ErrConflict:
+			app.conflictResponse(w, r, err)
+			return
+		default:
+			app.internalServerError(w, r, err)
+		}
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) unFollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	unfollowedUser := getUserFromContext(r)
+
+	// TODO: revert with user id from auth
+	var payload FollowUser
+	if err := ReadJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := app.store.Followers.Unfollow(r.Context(), payload.UserID, unfollowedUser.ID); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
+		if err != nil {
+			app.badRequestError(w, r, err)
+			return
+		}
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetById(ctx, userID)
+		if err != nil {
+			switch err {
+			case store.ErrNotFound:
+				app.badRequestError(w, r, err)
+				return
+			default:
+				app.internalServerError(w, r, err)
+				return
+			}
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromContext(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
