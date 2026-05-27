0eb234efe0083299aee219dc573c38e1b1297e57
JayTailor45 <tailorj64@gmail.com>
2026-05-27 10:35:11 +0530

    feat(feed): add filter by tags and search support

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -148,6 +148,7 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 }
 
 func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetadata, error) {
+	// TODO: add filter support for since and until date
 	query := `
 	SELECT 
 		p.id,
@@ -163,7 +164,10 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 	LEFT JOIN comments c ON c.post_id = p.id
 	LEFT JOIN users u ON p.user_id = u.id
 	JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
-	WHERE f.user_id = $1 OR p.user_id = $1
+	WHERE 
+		(f.user_id = $1 OR p.user_id = $1)	
+		AND (p.title ILIKE '%' || $4 || '%' OR p.content ILIKE '%' || $4 || '%')
+		AND (p.tags @> $5 OR $5 IS NULL)
 	GROUP BY p.id, u.username
 	ORDER BY p.created_at ` + fq.Sort + `
 	LIMIT $2
@@ -175,7 +179,7 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
 	if err != nil {
 		return nil, err
 	}
